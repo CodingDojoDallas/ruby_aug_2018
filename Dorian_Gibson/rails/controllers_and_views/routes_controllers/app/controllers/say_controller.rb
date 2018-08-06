a2bb8b53273d69107b1afecdf6b0fafc78bd9ce4
dgibs750 <doriangibson9663@gmail.com>
2018-08-06 13:09:22 -0500

    Added routes_controllers assignment

---
+++ Dorian_Gibson/rails/controllers_and_views/routes_controllers/app/controllers/say_controller.rb
@@ -0,0 +1,31 @@
+class SayController < ApplicationController
+  def index
+    render text: 'What do you want me to say?'
+  end
+
+  def hello
+    render text: 'Saying Hello!'
+  end
+
+  def hellodojo
+    render text: 'Hello CodingDojo!'
+  end
+
+  def helloJoe
+    render text: 'Saying Hello Joe!'
+  end
+
+  def helloMichael
+    redirect_to "/say/hello/joe/"
+  end
+
+  def times
+    session[:count] ? session[:count] += 1 : session[:count] = 1
+    render text: 'You have visited this url %d times' % session[:count]
+  end
+
+  def reset
+    session[:count] =nil
+    render text: 'Destroyed the session!'
+  end
+end
