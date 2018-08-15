09a886b15946e9d982b7e68e447a7ba3413dbd56
dgibs750 <doriangibson9663@gmail.com>
2018-08-15 17:37:53 -0500

    Added Dojo Secrets

---
+++ Dorian_Gibson/rails/DojoSecrets/app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  has_secure_password
+
+  validates :name, :presence => true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+  before_save :lowecase_fields
+
+  private
+    def lowecase_fields
+      self.email.downcase!
+    end
+
+end
