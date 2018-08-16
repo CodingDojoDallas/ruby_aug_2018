9f35a8e73b1372deec5ef2a58acaf70d453b6f36
dgibs750 <doriangibson9663@gmail.com>
2018-08-16 14:06:12 -0500

    User Features

--- Dorian_Gibson/rails/DojoSecrets/app/controllers/users_controller.rb
+++ Dorian_Gibson/rails/DojoSecrets/app/controllers/users_controller.rb
@@ -1,10 +1,50 @@
 class UsersController < ApplicationController
-  def new
-  end
 
   def show
   end
+  def new
+  end
+
+  def create
+    new_user = User.create(user_params)
+    if new_user.valid?
+      session[:user_id] = new_user.id
+      redirect_to "/users/#{new_user.id}"
+    else
+      flash[:errors] = new_user.errors.full_messages
+      redirect_to "/users/new"
+    end
+  end
+
 
   def edit
   end
+
+  def update
+    edit_user = current_user
+    edit_user.update(user_params)
+    if edit_user.valid?
+      redirect_to "/users/#{edit_user.id}"
+    else
+      flash[:errors] = edit_user.errors.full_messages
+      redirect_to "/users/#{edit_user.id}/edit"
+    end
+  end
+
+  def destroy
+    current_user.destroy
+    session[:user_id] = nil
+    redirect_to "/users/new"
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def authorize_user
+      unless session[:user_id].to_s == params[:id]
+        redirect_to "/users/#{session[:user_id].to_s}"
+      end
+    end
 end
